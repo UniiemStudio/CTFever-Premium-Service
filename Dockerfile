e4baec406c3b22dee2a047b3c6ab84ce7b3566c7
HoshinoSuzumi <master@uniiem.com>
2023-04-23 12:53:36 +0800

    optimize

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,22 @@
 FROM python:3.9.16-alpine as builder
+
 WORKDIR /app
 COPY . .
 
 ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
 
-RUN sed -i 's/dl-cdn.alpinelinux.org/mirrors.tuna.tsinghua.edu.cn/g' /etc/apk/repositories && pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
-RUN apk add --update gcc musl-dev python3-dev libffi-dev openssl-dev build-base && pip install --upgrade pip && pip wheel --no-cache-dir --no-deps --wheel-dir /data/python_wheels -r requirements.txt
+RUN sed -i 's/dl-cdn.alpinelinux.org/mirrors.tuna.tsinghua.edu.cn/g' /etc/apk/repositories && \
+    pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple && \
+    apk add --no-cache --upgrade gcc musl-dev python3-dev libffi-dev openssl-dev build-base && \
+    pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple && \
+    pip wheel --no-cache-dir --no-deps --wheel-dir /data/python_wheels -r requirements.txt
 
 FROM python:3.9.16-alpine
+
 LABEL maintainer="hoshinosuzumi"
+LABEL org.opencontainers.image.source=https://github.com/UniiemStudio/CTFever-Premium-Service
+
 WORKDIR /app
 COPY . .
 
@@ -17,8 +24,11 @@ ENV ENVIRONMENT production
 
 COPY --from=builder /data/python_wheels /data/python_wheels
 
-RUN sed -i 's/dl-cdn.alpinelinux.org/mirrors.tuna.tsinghua.edu.cn/g' /etc/apk/repositories && pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
-RUN pip install --no-cache /data/python_wheels/* && apk add unzip p7zip
+RUN sed -i 's/dl-cdn.alpinelinux.org/mirrors.tuna.tsinghua.edu.cn/g' /etc/apk/repositories && \
+    pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple && \
+    apk add --no-cache unzip p7zip && \
+    pip install --no-cache /data/python_wheels/* && \
+    rm -rf /data/python_wheels
 
 EXPOSE 9563
 
--- plugins/ziputil.py
+++ plugins/ziputil.py
@@ -1,8 +1,11 @@
 import asyncio
 import os
+import re
 import subprocess
+import time
 
 from fastapi import HTTPException, UploadFile
+from starlette.responses import FileResponse
 
 from core import Plugin
 
@@ -18,11 +21,16 @@ def is_pseudo_encryption(binary: bytes):
     :param binary:  Zip file in bytes
     :return:        1 if pseudo encrypted, 0 if true encrypted, -1 if not encrypted
     """
-    flag_pseudo_base = b'PK\x01\x02'
-    flag_true = b'\t\x00'
+    tag_content_block = b'\x50\x4B\x01\x02'
+    tag_file_header = b'\x50\x4B\x03\x04'
+    tag_file_footer = b'\x50\x4B\x05\x06'
+    flag_true = b'\x09\x00'
     flag_none = b'\x00\x00'
 
-    flag_index = binary.find(flag_pseudo_base)
+    print([m.start() for m in re.finditer(tag_file_header, binary)])
+    print([m.start() for m in re.finditer(tag_content_block, binary)])
+
+    flag_index = binary.find(tag_content_block)
     if binary[6:8] == flag_none and binary[flag_index + 8:flag_index + 10] == flag_none:
         result = -1
     elif binary[6:8] == flag_true and binary[flag_index + 8:flag_index + 10] == flag_none:
@@ -37,6 +45,15 @@ def is_pseudo_encryption(binary: bytes):
         [int(i) for i in binary[3:11]],
         [int(i) for i in binary[flag_index + 5:flag_index + 13]],
     ]
+
+
+# 将传入的 zip 文件 bytes 转为伪加密文件
+def convert2pseudo(binary: bytes, save_path: str):
+    flag_index = binary.find(b'\x50\x4B\x01\x02')
+    binary = binary[:6] + b'\x09\x00' + binary[8:flag_index + 8] + b'\x09\x00' + binary[flag_index + 10:]
+    with open(os.path.join(save_path, f'{int(time.time())}.zip'), 'wb') as fs:
+        fs.write(binary)
+    return os.path.join(save_path, f'{int(time.time())}.zip')
 
 
 class Ziputil(Plugin):
@@ -63,3 +80,16 @@ class Ziputil(Plugin):
             'assert': result,
             'characteristics': characteristics,
         }
+
+    async def convert_to_pseudo(self, params):
+        file: UploadFile = params.get('file', None)
+        if not file:
+            raise HTTPException(400, detail='No file provided')
+        file_content = await file.read()
+        if not is_zip(file_content):
+            raise HTTPException(400, detail='Not a zip file')
+        path = convert2pseudo(file_content, self._temp_dir)
+        return FileResponse(
+            filename=os.path.basename(path),
+            path=path,
+        )
